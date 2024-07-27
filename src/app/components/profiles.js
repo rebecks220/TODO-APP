7a1ae04e8edde989379adee1b0abcdbe87e94c02
B-mitchell <mitchellonuorah77@gmail.com>
2024-07-27 17:48:58 +0100

    todo code

--- src/app/components/profiles.js
+++ src/app/components/profiles.js
@@ -29,9 +29,6 @@ async function handleSubmit(e) {
     <div className='container text-center' >
 
       <h2 className='mt-[2rem] text-[2rem] font-bold text-center text-[#fff]'>Todo List</h2>
-
-
-
       <form className='mt-[.8rem]' onSubmit={handleSubmit}>
         <input placeholder='type your todo here...' className='p-[.6rem] outline-none border-[.2rem] border-blue-500 active:border-red-500 rounded' value={todos} onChange={(e) => setTodo(e.target.value)}/>
         <button type="submit" className='bg-[blue] text-[#fff] p-[.6rem] font-semibold ml-[.6rem] hover:scale-[115%] transition rounded-[1rem]'>Add Todo</button>
